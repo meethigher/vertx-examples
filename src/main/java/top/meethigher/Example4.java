c8e1da68b32b20b893ff8524014e5b00abc5007f
meethigher <meethigher@qq.com>
2024-11-03 15:45:42 +0800

    feat: httpserver通过自签名证书开启ssl

--- src/main/java/top/meethigher/Example4.java
+++ src/main/java/top/meethigher/Example4.java
@@ -2,8 +2,13 @@ package top.meethigher;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.net.PemKeyCertOptions;
 import io.vertx.ext.web.Router;
 import lombok.extern.slf4j.Slf4j;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Slf4j
 public class Example4 {
@@ -31,16 +36,25 @@ public class Example4 {
                 });
                 //`/*`表示匹配当前目录及子目录。注意与SpringWeb的区别。
                 router.route("/halo/*").handler(t -> {
-                    String s = t.request().absoluteURI();
-                    t.response().putHeader("Content-Type", "text/html")
-                            .end("<h1>Hello World</h1> " + s);
+                    // 开启chunked分片传输。content-length与transfer-encoding是矛盾的。
+                    // content-length需要服务器在内存中计算好内容长度，适用于数据较少时传输；而transfer-encoding则是分块流式传输，适用于大文件传输。
+                    // nginx自身不能直接开启chunked。需要借助插件或者实际后端服务。
+                    t.response().setChunked(true);
+                    t.response().end("hello world " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "\n"
+                            + t.request().absoluteURI());
                 });
                 //不传值表示匹配下面的所有内容
                 router.route().handler(t -> {
                     t.response().end("Hello World");
                 });
 
-                vertx.createHttpServer()
+                vertx.createHttpServer(new HttpServerOptions()
+                                .setSsl(true)
+                                .setKeyCertOptions(new PemKeyCertOptions()//使用自签名证书开启ssl
+                                        .addCertPath("/usr/local/nginx/conf/cert/certificate.pem")
+                                        .addKeyPath("/usr/local/nginx/conf/cert/private.key")
+                                )
+                        )
                         //注册路由
                         .requestHandler(router)
                         .listen(port).onComplete(re -> {
