5f16b740d25cd06a0e350d28eca678cec659a0a4
meethigher <meethigher@qq.com>
2025-03-23 20:22:28 +0800

    feat: httpserver新增静态资源路径

--- src/main/java/top/meethigher/Example4.java
+++ src/main/java/top/meethigher/Example4.java
@@ -3,8 +3,8 @@ package top.meethigher;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServerOptions;
-import io.vertx.core.net.PemKeyCertOptions;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
 import lombok.extern.slf4j.Slf4j;
 
 import java.text.SimpleDateFormat;
@@ -19,8 +19,11 @@ public class Example4 {
             public void start() throws Exception {
                 int port = 4321;
                 Router router = Router.router(vertx);
+
+                // order值越小，优先级越高
+
                 //注册接口
-                router.route("/test/test").handler(t -> {
+                router.route("/test/test").order(1).handler(t -> {
                     //接口逻辑
                     String testParam = t.request().getParam("test");
                     if (testParam == null) {
@@ -35,7 +38,7 @@ public class Example4 {
                     }
                 });
                 //`/*`表示匹配当前目录及子目录。注意与SpringWeb的区别。
-                router.route("/halo/*").handler(t -> {
+                router.route("/halo/*").order(2).handler(t -> {
                     // 开启chunked分片传输。content-length与transfer-encoding是矛盾的。
                     // content-length需要服务器在内存中计算好内容长度，适用于数据较少时传输；而transfer-encoding则是分块流式传输，适用于大文件传输。
                     // nginx自身不能直接开启chunked。需要借助插件或者实际后端服务。
@@ -44,16 +47,23 @@ public class Example4 {
                             + t.request().absoluteURI());
                 });
                 //不传值表示匹配下面的所有内容
-                router.route().handler(t -> {
+                router.route().order(3).handler(t -> {
                     t.response().end("Hello World");
                 });
 
+                // 注册静态资源路径，若访问的内容是404，则会回转到/*的路由上
+                StaticHandler staticHandler = StaticHandler.create("D:/3Develop/www")
+                        .setDirectoryListing(true)
+                        .setAlwaysAsyncFS(true)
+                        .setIndexPage("index.html");
+                router.route("/static/*").order(Integer.MIN_VALUE).handler(staticHandler);
+
                 vertx.createHttpServer(new HttpServerOptions()
-                                .setSsl(true)
-                                .setKeyCertOptions(new PemKeyCertOptions()//使用自签名证书开启ssl
-                                        .addCertPath("/usr/local/nginx/conf/cert/certificate.pem")
-                                        .addKeyPath("/usr/local/nginx/conf/cert/private.key")
-                                )
+//                                .setSsl(true)
+//                                .setKeyCertOptions(new PemKeyCertOptions()//使用自签名证书开启ssl
+//                                        .addCertPath("/usr/local/nginx/conf/cert/certificate.pem")
+//                                        .addKeyPath("/usr/local/nginx/conf/cert/private.key")
+//                                )
                         )
                         //注册路由
                         .requestHandler(router)
