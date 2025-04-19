b38ad8bafa5a7e39deb7f38616544b0d1b3001a9
meethigher <meethigher@qq.com>
2025-04-19 18:13:58 +0800

    feat: 对socket.handler和socket.pipeTo对于resume的理解

--- src/main/java/top/meethigher/Example15.java
+++ src/main/java/top/meethigher/Example15.java
@@ -1,11 +1,12 @@
 package top.meethigher;
 
-import io.vertx.core.Vertx;
-import io.vertx.core.VertxOptions;
+import io.vertx.core.*;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.net.NetClient;
 import io.vertx.core.net.NetClientOptions;
 import io.vertx.core.net.NetServerOptions;
 import io.vertx.core.net.NetSocket;
+import io.vertx.core.streams.WriteStream;
 
 import java.time.Duration;
 import java.util.concurrent.locks.LockSupport;
@@ -47,11 +48,50 @@ public class Example15 {
                                 NetSocket tsocket = tar.result();
                                 tsocket.pause();
                                 System.out.println(tsocket.localAddress() + " connected to " + tsocket.remoteAddress());
-                                socket.handler(b -> {
-                                    System.out.println(b);
+                                // 在编程时，handler注册的buffer，需要手动开启resume
+//                                socket.handler(b -> {
+//                                    System.out.println(b);
+//                                });
+                                // 在编程时，若使用了pipeTo，则不需要手动再去开启resume
+                                socket.pipeTo(new WriteStream<Buffer>() {
+                                    @Override
+                                    public WriteStream<Buffer> exceptionHandler(Handler<Throwable> handler) {
+                                        return null;
+                                    }
+
+                                    @Override
+                                    public Future<Void> write(Buffer data) {
+                                        System.out.println(data);
+                                        return null;
+                                    }
+
+                                    @Override
+                                    public void write(Buffer data, Handler<AsyncResult<Void>> handler) {
+                                        System.out.println(data);
+                                    }
+
+                                    @Override
+                                    public void end(Handler<AsyncResult<Void>> handler) {
+
+                                    }
+
+                                    @Override
+                                    public WriteStream<Buffer> setWriteQueueMaxSize(int maxSize) {
+                                        return null;
+                                    }
+
+                                    @Override
+                                    public boolean writeQueueFull() {
+                                        return false;
+                                    }
+
+                                    @Override
+                                    public WriteStream<Buffer> drainHandler(Handler<Void> handler) {
+                                        return null;
+                                    }
                                 });
                                 tsocket.resume();
-                                socket.resume();
+//                                socket.resume();
                             } else {
                                 tar.cause().printStackTrace();
                                 System.exit(1);
